bff3372582ca35105884a4ac5ecd568c6d5a5dae
JoaoJonusan <joaovitormartins338@gmail.com>
2026-07-16 19:32:57 -0300

    adição de armazenamento via arquivo json

--- arquivos.py
+++ arquivos.py
@@ -1,3 +1,13 @@
 import json
 
-def salvar_produtos()
+def salvar_produtos(lista, caminho="produtos.json"):
+        with open(caminho, "w", encoding="utf-8") as arquivo:
+            json.dump(lista, arquivo, ensure_ascii=False, indent=4)
+
+def carregar_produtos(caminho="produtos.json"):
+    try:
+        with open(caminho, "r", encoding="utf-8") as arquivo:
+            lista_produtos = json.load(arquivo)
+    except FileNotFoundError:
+        return []    
+    return lista_produtos    
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import produtos
 import interface
+import arquivos
 
 def iniciar():
     
-    lista_produto = []
-
-    produtos.adicionar_produto(lista_produto, produtos.cadastrar_produto("arroz", 17, 2))
-    produtos.adicionar_produto(lista_produto, produtos.cadastrar_produto("feijao", 7, 1))
-    produtos.adicionar_produto(lista_produto, produtos.cadastrar_produto("linguiça", 10, 3))
+    lista_produto = arquivos.carregar_produtos()
 
     interface.menu(lista_produto)
     interface.exibir_produtos(lista_produto)
+    
+    arquivos.salvar_produtos(lista_produto)
 
 if __name__ == "__main__":
     iniciar()

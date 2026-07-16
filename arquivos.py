ecb6f5aeb7ffbc395a62327bd04c96e756ac2838
JoaoJonusan <joaovitormartins338@gmail.com>
2026-07-16 20:18:03 -0300

    arquivo de produtos adicionado a pasta do projeto dentro da pasta dados

--- arquivos.py
+++ arquivos.py
@@ -1,13 +1,29 @@
 import json
+from pathlib import Path
 
-def salvar_produtos(lista, caminho="produtos.json"):
-        with open(caminho, "w", encoding="utf-8") as arquivo:
-            json.dump(lista, arquivo, ensure_ascii=False, indent=4)
+PASTA_PROJETO = Path(__file__).resolve().parent
+PASTA_DADOS = PASTA_PROJETO / "dados"
+ARQUIVO_PRODUTOS = PASTA_DADOS / "produtos.json"
 
-def carregar_produtos(caminho="produtos.json"):
+def salvar_produtos(lista, caminho=ARQUIVO_PRODUTOS):
+    caminho = Path(caminho)
+
+    caminho.parent.mkdir(parents=True, exist_ok=True)
+    
+    with open(caminho, "w", encoding="utf-8") as arquivo:
+        json.dump(lista, arquivo, ensure_ascii=False, indent=4)
+
+def carregar_produtos(caminho=ARQUIVO_PRODUTOS):
+    caminho = Path(caminho)
+    
     try:
         with open(caminho, "r", encoding="utf-8") as arquivo:
-            lista_produtos = json.load(arquivo)
+            return json.load(arquivo)
+            
     except FileNotFoundError:
-        return []    
-    return lista_produtos    
+        return []
+
+    except json.JSONDecodeError:
+        print("O arquivo de produtos está vazio ou inválido.")
+        return []
+    

944c631dbf8f6cf219954030130e1b9e413baa14
JoaoJonusan <joaovitormartins338@gmail.com>
2026-07-18 23:37:03 -0300

    adição de verificação da entrada de dados direto no __init__ da classe

--- app/modelos.py
+++ app/modelos.py
@@ -2,7 +2,14 @@
 class Produto:
 
     def __init__(self, nome, preco, quantidade):
-        self.nome = nome
+        if not nome.strip():
+            raise ValueError("O nome nao pode ficar vazio")
+        if preco <= 0:
+            raise ValueError("O preço deve ser maior que zero")
+        if quantidade < 0:
+            raise ValueError("A quantidade não pode ser negativa")
+
+        self.nome = nome.strip()
         self.preco = preco
         self.quantidade = quantidade
     
